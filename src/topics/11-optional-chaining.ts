832a090c6085a40eb52315328a1d2d23e44eccc0
Alan Rueda <josuerueda1998@gmail.com>
2023-10-11 23:18:57 -0500

    typescript-intro

---
+++ src/topics/11-optional-chaining.ts
@@ -0,0 +1,26 @@
+export interface Passenger {
+  name: string;
+  children?: string[];
+}
+
+const passenger1: Passenger = {
+  name:'Alan'
+}
+
+const passenger2: Passenger = {
+  name:'Josue',
+  children: ['Naty','Juan']
+}
+
+const returnChildren = (passenger: Passenger): number => {
+
+  //if(!passenger.children) return 0;
+
+  const howManyChildren = passenger.children?.length||0;
+  //const howManyChildren = passenger.children!.length;
+  console.log(passenger.name,howManyChildren)
+
+  return howManyChildren;
+}
+
+returnChildren(passenger1);
